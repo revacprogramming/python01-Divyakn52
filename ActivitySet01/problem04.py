ca8a050b41c8f299b99aec4ae4043ba0f197e74f
Divzz <97013125+Divyakn52@users.noreply.github.com>
2022-05-23 03:48:46 +0000

    04 completed

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -5,11 +5,8 @@ h = float(hrs)
 rate= input("enter rate:")
 r=float(rate)
 if h>40 :
-    reg=h*r
-    ovr=(h-40)*(r*0.5)
-    pay=reg+ovr
-    print(pay)
-else :
+    print(r*40+(h-40)*1.5*r)
+else:
     pay=h*r
     print(pay)
     
